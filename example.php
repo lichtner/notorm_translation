70be3506ff30c7348a99571dd16ea69ad12e1803
Marek Lichtner <marek@licht.sk>
2011-07-20 13:09:11 +0200

    update example.php

--- example.php
+++ example.php
@@ -1,15 +1,22 @@
 <?php
 
+/**
+ * NotORM translation example usage
+ *
+ * 1. create mysql database notorm_trans from file tests/database-for-tests.sql
+ * 2. set up your pdo
+ */
+
 // set path for notorm
 include_once dirname(__FILE__) . '/../notorm/NotORM.php';
-include_once dirname(__FILE__) . '/notorm_translator/Trans.php';
+include_once dirname(__FILE__) . '/notorm_translation/Trans.php';
 
 $connection = new PDO('mysql:dbname=notorm_trans', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES 'UTF8'"));
-$structure = new NotORM_Trans_Structure($connection);
+$structure = new NotORM_Structure_Trans($connection);
 
 $db = new NotORM_Trans($connection, $structure, new NotORM_Cache_Database($connection));
 // $db = new NotORM_Trans($connection, $structure, new NotORM_Cache_Session);
-$db->setRowClass('NotORM_Trans_Row');
+$db->setRowClass('NotORM_Row_Trans');
 $db->setLanguages('sk', 'en');
 //$db->debug = true;
 
